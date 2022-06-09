c460bf2c13330f3cceffd544daef8e44fc0d98e1
urxt <80124020+urxt@users.noreply.github.com>
2022-06-09 18:56:20 -0400

    refactoring

--- printer.hs
+++ printer.hs
@@ -1,19 +1,22 @@
 -- Output html to file
 
 -- type signatures
-html_ :: String -> String
-title_ :: String -> String
-body_ :: String -> String
-head_ :: String -> String
-makeHtml :: String -> String -> String
 
 main = putStrLn myHtml 
 
+html_ :: String -> String
 html_ content = "<html>" <> content <> "</html>"
+
+title_ :: String -> String
 title_ content = "<html>" <> content <> "</title>"
+
+head_ :: String -> String
 head_ content = "<head>" <> content <> "</head>"
+
+body_ :: String -> String
 body_ content = "<body>" <> content <> "</body>"
 
 myHtml = makeHtml "Title" "Content."
 
+makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
