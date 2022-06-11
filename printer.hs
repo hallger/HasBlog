d1049c2ed78f37e6245c05557221947d72a78e37
urxt <80124020+urxt@users.noreply.github.com>
2022-06-10 20:34:36 -0400

    refactoring

--- printer.hs
+++ printer.hs
@@ -1,8 +1,8 @@
 -- Output html to file
 
-Html :: String -> Html
 newtype Html = Html String
 newtype Structure = Structure String
+type Title = String
 
 main :: IO ()
 main = putStrLn myHtml
@@ -11,32 +11,27 @@ el :: String -> String -> String
 el tag content = 
     "<" <> tag ">" <> content <> "</" <> tag <> ">"
 
--- p_ :: String -> String
--- p_ = el "p"
+p_ :: String -> String
+p_ = Structure . el "p"
 
 h1_ :: String -> String 
-h1_ = el "h1"
+h1_ = Structure . el "h1"
 
-html_ :: String -> String 
-html_ = el "html"
+html_ :: Title -> Structure -> Html
+html_ title content = 
+    Html 
+        ( el "html" 
+            ( el "head" (el "title" title)
+                <> el "body" (getStructureString content)
+            )
+        )
 
-title_ :: String -> String
-title_ = el "title"
-
-head_ :: String -> String
-head_ = el "title"
-
-body_ :: String -> String 
-body_ = el "body" 
-
-myHtml :: String
+myHtml :: Html
 myHtml = makeHtml "Title" "Content."
 
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
-p_ :: String -> String
-p_ = Structure . el "p"
 
 -- editing functions
 append_ :: Structure -> Structure -> Structure 
