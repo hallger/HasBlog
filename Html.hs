86582eb33d82772a3e6e7ab2d21e6225b04f67c0
urxt <80124020+urxt@users.noreply.github.com>
2022-06-11 02:06:38 -0400

    refactored module and printer file

--- Html.hs
+++ Html.hs
@@ -10,3 +10,39 @@ module Html
     )
     where
 
+newtype Html = Html String
+newtype Structure = Structure String
+type Title = String
+
+html_ :: Title -> Structure -> Html
+html_ title content = 
+    Html 
+        ( el "html" 
+            ( el "head" (el "title" title)
+                <> el "body" (getStructureString content)
+            )
+        )
+
+el :: String -> String -> String
+el tag content = 
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">" 
+
+p_ :: String -> Structure
+p_ = Structure . el "p" 
+
+h1_ :: String -> Structure 
+h1_ = Structure . el "h1"
+
+append_ :: Structure -> Structure -> Structure 
+append_ c1 c2 =
+    Structure (getStructureString c1 <> getStructureString c2) 
+
+getStructureString :: Structure -> String 
+getStructureString content = 
+    case content of  
+        Structure str -> str 
+
+render :: Html -> String
+render html = 
+    case html of
+        Html str -> str 
--- printer.hs
+++ printer.hs
@@ -1,11 +1,7 @@
--- Output html to file
+import Html
 
 main :: IO ()
 main = putStrLn (render myHtml)
-
-newtype Html = Html String
-newtype Structure = Structure String
-type Title = String
 
 myHtml :: Html
 myHtml = 
@@ -19,35 +15,3 @@ myHtml =
             )
         )
 
-html_ :: Title -> Structure -> Html
-html_ title content = 
-    Html 
-        ( el "html" 
-            ( el "head" (el "title" title)
-                <> el "body" (getStructureString content)
-            )
-        )
-
-el :: String -> String -> String
-el tag content = 
-    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-p_ :: String -> Structure
-p_ = Structure . el "p"
-
-h1_ :: String -> Structure 
-h1_ = Structure . el "h1"
-
-append_ :: Structure -> Structure -> Structure 
-append_ c1 c2 =
-    Structure (getStructureString c1 <> getStructureString c2)
-
-getStructureString :: Structure -> String 
-getStructureString content = 
-    case content of 
-        Structure str -> str
-
-render :: Html -> String
-render html = 
-    case html of
-        Html str -> str
