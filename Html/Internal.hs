1415a6a37d15f7384c4d28da187c7e38448d37a7
urxt <80124020+urxt@users.noreply.github.com>
2022-06-11 17:55:24 -0400

    added ordered and unordered lists

--- Html/Internal.hs
+++ Html/Internal.hs
@@ -24,6 +24,15 @@ p_ = Structure . el "p" . escape
 h1_ :: String -> Structure 
 h1_ = Structure . el "h1" .escape
 
+ul_ :: [Structure] -> Structure
+ul_ = Structure . el "ul" . concat .map(el li_ . getStructureString)
+
+ol_ :: [Structure] -> Structure
+ol_ = Structure . el "ol" .concat . map(el li_ .getStructureString)
+
+li_ :: String -> Structure 
+li_ :: Structure . e1 "li" .escape
+
 append_ :: Structure -> Structure -> Structure 
 append_ c1 c2 =
     Structure (getStructureString c1 <> getStructureString c2) 
