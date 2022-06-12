c6a2729bae79576335ca0f495ea409cf8fe6fd4c
urxt <80124020+urxt@users.noreply.github.com>
2022-06-11 21:57:29 -0400

    added markup module

---
+++ Markup.hs
@@ -0,0 +1,17 @@
+module Markup
+    ( Document
+    , Structure(..)
+    )
+where
+
+import Numeric.Natural
+
+type Document 
+    = [Structure]
+
+data Structure  
+    = Heading Natural String
+    | Paragraph String
+    | UnorderedList [String]
+    | OrderedList [String]
+
