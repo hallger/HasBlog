76ac4cd01c721498af277a6e573ada6bbac94b1e
urxt <80124020+urxt@users.noreply.github.com>
2022-06-13 13:26:21 -0400

    added confirm function for user input

--- Main.hs
+++ Main.hs
@@ -5,6 +5,18 @@ import System.Environment (getArgs)
 import qualified Html
 import qualified Markup
 
+
 process :: Html.Title -> String -> String
 process title = Html.render . convert title . Markup.parse
+
+confirm :: IO Bool
+confirm = 
+    putStrLn "Are you sure? (Y/n)" *>
+        getLine >>= \answer -> 
+            case answer of  
+                "y" -> pure True
+                "n" -> pure False
+                _   -> putStrLn "Invalid response." *>
+                    confirm
+
 
