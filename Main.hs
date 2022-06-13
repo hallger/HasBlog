30566acdcf2a09c13cc622faac9553199f3db2f0
urxt <80124020+urxt@users.noreply.github.com>
2022-06-13 13:54:31 -0400

    added do notation to main

--- Main.hs
+++ Main.hs
@@ -7,44 +7,44 @@ import qualified Markup
 import Convert (convert)
 
 main :: IO ()
-main = 
-    getArgs >>= \args ->
-        case args of 
-            -- No arguments given
-            [] -> 
-                getContents >>= \content -> 
-                    putStrLn (process "Empty" content)
+main = do
+    args <- getArgs 
+    case args of 
+        -- No arguments given
+        [] -> do
+            content <- getContents
+                putStrLn (process "Empty" content)
 
-            -- IO file paths given as args
-            [input, output] -> 
-                readFile input >>= \content -> 
-                    doesFileExist output >>= \exists -> 
-                        let 
-                            writeResult = writeFile output (process input content)
-                        in 
-                            if exists 
-                                then whenIO confirm writeResult
-                                else writeResult
+        -- IO file paths given as args
+        [input, output] -> do
+            content <- readFile input
+            exists <- doesFileExist output
+            let 
+                writeResult = writeFile output (process input content)
+            if exists 
+                then whenIO confirm writeResult
+                else writeResult
 
-            _ -> 
-                putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
+        _ -> 
+            putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
 
 process :: Html.Title -> String -> String
 process title = Html.render . convert title . Markup.parse
 
 confirm :: IO Bool
-confirm = 
-    putStrLn "Are you sure? (Y/n)" *>
-        getLine >>= \answer -> 
-            case answer of  
-                "y" -> pure True
-                "n" -> pure False
-                _   -> putStrLn "Invalid response." *>
-                    confirm
+confirm = do
+    putStrLn "Are you sure? (Y/n)"
+    answer <- getLine
+    case answer of
+        "y" -> pure True
+        "n" -> pure False
+        _   -> do
+            putStrLn "Invalid response."
+            confirm
 
 
 whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action = 
+whenIO cond action = do
     cond >>= \result -> 
         if result 
             then action
