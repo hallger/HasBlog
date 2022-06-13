337269d1c1165b28677e94f64b37e6ede51fe5f4
urxt <80124020+urxt@users.noreply.github.com>
2022-06-12 20:10:54 -0400

    added parsing function to markup

--- Markup.hs
+++ Markup.hs
@@ -1,6 +1,7 @@
 module Markup
     ( Document
     , Structure(..)
+    , parse
     )
 where
 
@@ -14,4 +15,55 @@ data Structure
     | Paragraph String
     | UnorderedList [String]
     | OrderedList [String]
+    deriving (Eq, Show)
+
+trim :: String -> String
+trim = unwords . words
+
+parse :: String -> Document 
+parse = parseLines Nothing . lines
+
+parseLines :: Maybe Structure -> [String] -> Document
+parseLines context txts =
+    case txts of
+        [] -> maybeToList context
+
+        ('*': ' ': line) : rest -> 
+            maybe id (:) context (Heading 1 (trim line) : parseLines Nothing rest)
+
+        ('-' : ' ' : line) : rest -> 
+            case context of 
+                Just (UnorderedList list) ->
+                    parseLines (Just (UnorderedList (list <> [trim line]))) rest
+
+                _ ->
+                    maybe id (:) context (parseLines (Just (UnorderedList [trim line])) rest)
+
+        ('#' : ' ' : line) : rest ->
+            case context of 
+                Just (OrderedList list) -> 
+                    parseLines (Just (OrderedList (list <> [trim line]))) rest
+
+
+                _ -> 
+                    maybe id (:) context (parseLines (Just (OrderedList [trim line])) rest)
+
+        currentLine : rest -> 
+            let 
+                line = trim currentLine
+            in 
+                if line == "" 
+                    then 
+                        maybe id (:) context (parseLines Nothing rest)
+                    else 
+                        case context of 
+                            Just (Paragraph paragraph) -> 
+                                parseLines (Just (Paragraph (unwords [paragraph, line]))) rest
+                            _ -> 
+                                maybe id (:) context (parseLines (Just (Paragraph line)) rest)
+
+
+
+
+
 
